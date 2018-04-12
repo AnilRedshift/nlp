332b792ff8344b7a05ce52fa7bdd14b43136fc61
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-04-12 01:18:38 -0700

    Add corpora tests

--- src/text/en/inflect.test.js
+++ src/text/en/inflect.test.js
@@ -1,7 +1,18 @@
+import csv from 'csvtojson';
 import { singularize } from './inflect';
+import wordforms from '../../../test/corpora/wordforms-en-celex.csv';
 
 describe('singularize', () => {
   test('words ending in s should be singular', () => {
     expect(singularize('appointments')).toBe('appointment');
+  });
+
+  test('corpora matches', (done) => {
+    csv({ noheader: true, headers: ['singular', 'plural'] })
+      .fromString(wordforms)
+      .on('json', ({ singular, plural }) => {
+        expect(singularize(plural)).toBe(singular);
+      })
+      .on('done', done);
   });
 });
